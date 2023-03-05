3eaf0583fb27c6a63f91937c24eff5d0fa6ae655
ANDREAS VLCEK <andreas.vlcek@googlemail.com>
2023-03-05 12:41:23 +0100

    making step definition fail again

--- Chapter 2/calculator/features/step_definitions/calculator_steps.rb
+++ Chapter 2/calculator/features/step_definitions/calculator_steps.rb
@@ -7,6 +7,6 @@ When(/^the calculator is run$/) do
 	raise('Command failed!') unless $?.success?
 end
 
-Then(/^the output should be "([^"]*)"$/) do |arg1|
-	pending # express the regex above with bthe code you wish you had
+Then(/^the output should be "([^"]*)"$/) do |expected_output|
+	expect(@output).to eq expected_output
 end
