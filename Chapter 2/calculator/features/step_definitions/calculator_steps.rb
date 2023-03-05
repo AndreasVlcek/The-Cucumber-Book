552f7ec92feed7093eed6bd62a446b1c592544e6
ANDREAS VLCEK <andreas.vlcek@googlemail.com>
2023-03-05 12:38:37 +0100

    added calc app and fixed error in step definitions

--- Chapter 2/calculator/features/step_definitions/calculator_steps.rb
+++ Chapter 2/calculator/features/step_definitions/calculator_steps.rb
@@ -7,6 +7,6 @@ When(/^the calculator is run$/) do
 	raise('Command failed!') unless $?.success?
 end
 
-Then(/^the output should be "([^"]*)"$/) do
+Then(/^the output should be "([^"]*)"$/) do |arg1|
 	pending # express the regex above with bthe code you wish you had
 end
